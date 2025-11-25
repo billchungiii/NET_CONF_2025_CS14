1e8dcce5061e3a976e9d4c5ba96077f86fd3fcef
Bill Chung <bill_chungiii@hotmail.com>
2025-11-25 23:51:21 +0800

    加入專案檔案。

---
+++ Field_Block_Property001/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Field_Block_Property001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Null_Conditional001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+
+namespace Null_Conditional001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            // before 
+            if (person.Address != null)
+            {
+                 person.Address.City = "New York";
+            }
+
+            // after
+            person.Address?.City = "New York";  
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Address Address { get; set; }
+    }
+    public class Address
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+    }
+}
