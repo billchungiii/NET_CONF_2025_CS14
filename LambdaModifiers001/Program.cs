ba469bb8b00ddd5bf89857cc78e9b8e3b446e7b4
Bill Chung <bill_chungiii@hotmail.com>
2025-11-26 18:44:00 +0800

    add lambda modifiler proj

---
+++ LambdaModifiers001/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LambdaModifiers001
+{
+    delegate bool TryParse<T>(string s, out T result);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // before C# 14.0, 使用 ref/out/int/ref readonly/scoped 修飾詞的 Lambda 參數必須顯式指定型別
+            TryParse<int> tryParse = (string text, out int result) => int.TryParse(text, out result);
+
+            // 從 C# 14.0 開始，可以省略型別，編譯器會自動推斷
+            TryParse<int> tryParse2 = (text, out result) => int.TryParse(text, out result);
+
+            
+        }
+    }
+}
