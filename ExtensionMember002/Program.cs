6a42aac97774d4a85763cd582a2cab122be43471
Bill Chung <bill_chungiii@hotmail.com>
2025-11-27 09:52:07 +0800

    排版

--- ExtensionMember002/Program.cs
+++ ExtensionMember002/Program.cs
@@ -7,19 +7,24 @@
             /* 語意上較不明顯, 需要記得 MyExtensions 這個類別 */
             _ = MyExtensions.Range(5, 10);
             /* 語意上較明顯, 直覺使用 int 的成員 */
-            var result = int.Range(5, 10);
+            var result = int.Range(5, 10);           
             Console.WriteLine(string.Join(", ", result));
+
+            var comparer = int.EqualityComparer;
         }
     }
 
     public static class MyExtensions
     {
-        extension(int value)
+        extension(int source)
         {
             public static IEnumerable<int> Range(int start, int count)
             {
                 return Enumerable.Range(start, count);
             }
+
+            public static IEqualityComparer<int> EqualityComparer => EqualityComparer<int>.Default;
         }
     }
 }
+
